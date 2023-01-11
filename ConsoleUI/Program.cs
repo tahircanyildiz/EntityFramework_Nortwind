67645b71a2c22fee5065d2877fb77a6adb7b0929
Tahir Can Yıldız <tahircanyildiz@hotmail.com>
2023-01-11 21:32:24 +0300

    Proje dosyası ekle.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EfProductDal efProductDal = new EfProductDal();
+            ProductManager productManager = new ProductManager(efProductDal);
+
+            for (int i = 0; i < 5; i++)
+            {
+                foreach (var product in productManager.GetAllByCategoryId(i))
+                {
+                    Console.WriteLine(product.CategoryId + " " + product.ProductName);
+                }
+            }
+            
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NortwindContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //DB tablolarını proje classları ile ilişkilendirmek için
+    public class NortwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Integrated Security=True");
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+
+
+    }
+}
